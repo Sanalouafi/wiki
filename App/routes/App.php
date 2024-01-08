85fedbb2bf1d5b41991481fdf7871fcf28a6b6b0
Sanalouaf <sanalouafi2003@gmail.com>
2024-01-08 22:28:40 +0100

    add routes and htaccess

---
+++ App/routes/App.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once '../../vendor/autoload.php';
+
+use App\routes\Router;
+
+$router = new Router();
+
+$router->setRoutes([
+    'GET' => [
+        'signup' => ['AuthController', 'signup'],
+        'signin' => ['AuthController', 'login'],
+
+    ],
+    'POST' => [
+        'signup' => ['AuthController', 'registerUser'],
+        'signin' => ['AuthController', 'authenticateUser'],
+
+    ],
+]);
+
+if (isset($_GET['url'])) {
+    $uri = trim($_GET['url'], '/');
+
+    $methode = $_SERVER['REQUEST_METHOD'];
+
+    try {
+        $route = $router->getRoute($methode, $uri);
+
+        if ($route) {
+            list($controllerName, $methodName) = $route;
+
+            $controllerClass = 'App\\controllers\\' . ucfirst($controllerName);
+
+            $controller = new $controllerClass();
+
+            if ($methodName) {
+                if (method_exists($controller, $methodName)) {
+                    $controller->$methodName();
+                } else {
+                    throw new Exception('Method not found in controller.');
+                }
+            } else {
+                $controller->index();
+            }
+        } else {
+            throw new Exception('Route not found.');
+        }
+    } catch (Exception $e) {
+        echo 'Caught exception: ', $e->getMessage(), "\n";
+    }
+}
